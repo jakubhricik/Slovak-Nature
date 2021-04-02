92e87cc07603d6b4affbcb354082d213800b5b7e
Jakub Hricik <jakub.hricik@student.tuke.sk>
2021-04-02 16:26:17 +0200

    Initial Commit

---
+++ Slovak-Nature/Slovak_NatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slovak_NatureApp.swift
+//  Slovak-Nature
+//
+//  Created by Jakub Hricik on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Slovak_NatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
